7af5bd6db641128133e88d120d5088e4f6942083
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-12-22 01:03:49 +0100

    Add a link

---
+++ Src/xbc-test/xbc-test/Form1.cs
@@ -0,0 +1,89 @@
+﻿using controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xbc_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+        private static extern uint TimeBeginPeriod(uint ms);
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+        private static extern uint TimeEndPeriod(uint ms);
+        [DllImport("ntdll.dll", EntryPoint = "NtSetTimerResolution")]
+        private static extern void NtSetTimerResolution(uint DesiredResolution, bool SetResolution, ref uint CurrentResolution);
+        private static uint CurrentResolution = 0;
+        private static bool controller1_send_back, controller1_send_start, controller1_send_A, controller1_send_B, controller1_send_X, controller1_send_Y, controller1_send_up, controller1_send_left, controller1_send_down, controller1_send_right, controller1_send_leftstick, controller1_send_rightstick, controller1_send_leftbumper, controller1_send_rightbumper, controller1_send_xbox;
+        private static double controller1_send_leftstickx, controller1_send_leftsticky, controller1_send_rightstickx, controller1_send_rightsticky, controller1_send_lefttriggerposition, controller1_send_righttriggerposition;
+        public bool running;
+        private static int inc = 0;
+        private ScpBus scp = new ScpBus();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TimeBeginPeriod(1);
+            NtSetTimerResolution(1, true, ref CurrentResolution);
+            SetProcessPriority();
+            Task.Run(() => Start());
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                running = false;
+                Thread.Sleep(100);
+                scp.UnLoadController();
+            }
+            catch { }
+        }
+        private void SetProcessPriority()
+        {
+            using (Process p = Process.GetCurrentProcess())
+            {
+                p.PriorityClass = ProcessPriorityClass.RealTime;
+            }
+        }
+        private void Start()
+        {
+            running = true;
+            scp.LoadController();
+            Task.Run(() => taskX());
+        }
+        private void taskX()
+        {
+            for (; ; )
+            {
+                if (!running)
+                    break;
+                inc++;
+                if (inc <= 200 & inc >= 100)
+                {
+                    controller1_send_A = true;
+                    controller1_send_rightstickx = 16000;
+                }
+                else
+                {
+                    controller1_send_A = false;
+                    controller1_send_rightstickx = 0;
+                }
+                if (inc > 200)
+                    inc = 0;
+                scp.SetController(controller1_send_back, controller1_send_start, controller1_send_A, controller1_send_B, controller1_send_X, controller1_send_Y, controller1_send_up, controller1_send_left, controller1_send_down, controller1_send_right, controller1_send_leftstick, controller1_send_rightstick, controller1_send_leftbumper, controller1_send_rightbumper, controller1_send_leftstickx, controller1_send_leftsticky, controller1_send_rightstickx, controller1_send_rightsticky, controller1_send_lefttriggerposition, controller1_send_righttriggerposition, controller1_send_xbox);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
