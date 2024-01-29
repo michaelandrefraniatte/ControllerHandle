9c09066116d13dcfb2288ed697b681a97daf08e4
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-01-29 19:10:30 +0100

    Add a link

---
+++ Src/ObjectHandle/Object/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Object
+{
+    public class Class1
+    {
+        public static List<int> staticlist = new List<int>();
+        public List<int> list = new List<int>();
+        public void ShowStaticList()
+        {
+            foreach (int i in staticlist)
+            {
+                MessageBox.Show("From static list: " + i.ToString());
+            }
+        }
+        public void AddToStaticList(int i)
+        {
+            staticlist.Add(i);
+        }
+        public void ShowList()
+        {
+            foreach (int i in list)
+            {
+                MessageBox.Show("From list: " + i.ToString());
+            }
+        }
+        public void AddToList(int i)
+        {
+            list.Add(i);
+        }
+    }
+}
---
+++ Src/ObjectHandle/ObjectHandle/Form1.cs
@@ -0,0 +1,39 @@
+﻿using Object;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectHandle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i = 0;
+        Class1 obj1 = new Class1();
+        Class1 obj2 = new Class1();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i++;
+            obj1.AddToList(i);
+            obj2.AddToList(i);
+            obj1.AddToStaticList(i);
+            obj2.AddToStaticList(i);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            obj1.ShowList();
+            obj2.ShowList();
+            obj1.ShowStaticList();
+            obj2.ShowStaticList();
+        }
+    }
+}
