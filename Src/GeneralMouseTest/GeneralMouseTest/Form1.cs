063c640f652de26ffaa71d2b55fe231e4413f831
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-02-22 01:31:23 +0100

    Update links

--- Src/GeneralMouseTest/GeneralMouseTest/Form1.cs
+++ Src/GeneralMouseTest/GeneralMouseTest/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Xna.Framework.Input;
 
 namespace GeneralMouseTest
 {
@@ -16,15 +17,50 @@ namespace GeneralMouseTest
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        private bool closed = false;
+        private MouseState mousestate;
+        public bool MouseButtons0;
+        public bool MouseButtons1;
+        public bool MouseButtons2;
+        public bool MouseButtons3;
+        public bool MouseButtons4;
+        public int MouseAxisX;
+        public int MouseAxisY;
+        public int MouseAxisZ;
+        public void Form1_Load(object sender, EventArgs e)
         {
-
+            mousestate = Mouse.GetState();
+            Task.Run(() => taskEmulate());
         }
-
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        private void taskEmulate()
         {
-
+            while (!closed)
+            {
+                mousestate = Mouse.GetState();
+                MouseButtons0 = mousestate.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed;
+                MouseButtons1 = mousestate.RightButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed;
+                MouseButtons2 = mousestate.MiddleButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed;
+                MouseButtons3 = mousestate.XButton1 == Microsoft.Xna.Framework.Input.ButtonState.Pressed;
+                MouseButtons4 = mousestate.XButton2 == Microsoft.Xna.Framework.Input.ButtonState.Pressed;
+                MouseAxisX = mousestate.X;
+                MouseAxisY = mousestate.Y;
+                MouseAxisZ = mousestate.ScrollWheelValue;
+                string str = "MouseAxisX : " + MouseAxisX + Environment.NewLine;
+                str += "MouseAxisY : " + MouseAxisY + Environment.NewLine;
+                str += "MouseAxisZ : " + MouseAxisZ + Environment.NewLine;
+                str += "MouseButtons0 : " + MouseButtons0 + Environment.NewLine;
+                str += "MouseButtons1 : " + MouseButtons1 + Environment.NewLine;
+                str += "MouseButtons2 : " + MouseButtons2 + Environment.NewLine;
+                str += "MouseButtons3 : " + MouseButtons3 + Environment.NewLine;
+                str += "MouseButtons4 : " + MouseButtons4 + Environment.NewLine;
+                str += Environment.NewLine;
+                this.label1.Text = str;
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+        public void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            closed = true;
         }
     }
 }
