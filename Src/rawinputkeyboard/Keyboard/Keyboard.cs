5c311adad3770bafd385aa7c52fad0ae2d09496e
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-03-06 14:57:35 +0100

    Remove useless

--- Src/rawinputkeyboard/Keyboard/Keyboard.cs
+++ Src/rawinputkeyboard/Keyboard/Keyboard.cs
@@ -1,32 +1,20 @@
-﻿using System;
-using RawInput_dll;
-using System.Diagnostics;
+﻿using RawInput_dll;
 using System.Globalization;
 using System.Windows.Forms;
-
 
 namespace Keyboard
 {
     public partial class Keyboard : Form
     {
         private readonly RawInput _rawinput;
-        
-        const bool CaptureOnlyInForeground = true;
-        // Todo: add checkbox to form when checked/uncheck create method to call that does the same as Keyboard ctor 
-
+        const bool CaptureOnlyInForeground = false;
         public Keyboard()
         {
             InitializeComponent();
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-
             _rawinput = new RawInput(Handle, CaptureOnlyInForeground);
-           
-            _rawinput.AddMessageFilter();   // Adding a message filter will cause keypresses to be handled
-            Win32.DeviceAudit();            // Writes a file DeviceAudit.txt to the current directory
-
+            _rawinput.AddMessageFilter();
             _rawinput.KeyPressed += OnKeyPressed;   
         }
-
         private void OnKeyPressed(object sender, RawInputEventArg e)
         {
             lbHandle.Text = e.KeyPressEvent.DeviceHandle.ToString();
@@ -39,34 +27,10 @@ namespace Keyboard
             lbSource.Text = e.KeyPressEvent.Source;
             lbKeyPressState.Text = e.KeyPressEvent.KeyPressState;
             lbMessage.Text = string.Format("0x{0:X4} ({0})", e.KeyPressEvent.Message);
-           
-            //switch (e.KeyPressEvent.Message)
-            //{
-            //    case Win32.WM_KEYDOWN:
-            //        Debug.WriteLine(e.KeyPressEvent.KeyPressState);
-            //        break;
-            //     case Win32.WM_KEYUP:
-            //        Debug.WriteLine(e.KeyPressEvent.KeyPressState);
-            //        break;
-            //}
         }
-
         private void Keyboard_FormClosing(object sender, FormClosingEventArgs e)
         {
             _rawinput.KeyPressed -= OnKeyPressed;
-        }
-
-        private static void CurrentDomain_UnhandledException(Object sender, UnhandledExceptionEventArgs e)
-        {
-            var ex = e.ExceptionObject as Exception;
-
-            if (null == ex) return;
-
-            // Log this error. Logging the exception doesn't correct the problem but at least now
-            // you may have more insight as to why the exception is being thrown.
-            Debug.WriteLine("Unhandled Exception: " + ex.Message);
-            Debug.WriteLine("Unhandled Exception: " + ex);
-            MessageBox.Show(ex.Message);
         }
     }
 }
--- Src/rawinputmouse/Mouse/Form1.cs
+++ Src/rawinputmouse/Mouse/Form1.cs
@@ -1,32 +1,19 @@
-﻿using System;
-using RawInput_dll;
-using System.Diagnostics;
+﻿using RawInput_dll;
 using System.Globalization;
 using System.Windows.Forms;
-using System.Runtime.Remoting.Messaging;
 
 namespace Mouse
 {
     public partial class Form1 : Form
     {
         private readonly RawInput _rawinput;
-
-        const bool CaptureOnlyInForeground = true;
-        // Todo: add checkbox to form when checked/uncheck create method to call that does the same as Keyboard ctor 
-
+        const bool CaptureOnlyInForeground = false;
         public Form1()
         {
             InitializeComponent();
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-
             _rawinput = new RawInput(Handle, CaptureOnlyInForeground);
-
-            _rawinput.AddMessageFilter();   // Adding a message filter will cause keypresses to be handled
-            Win32.DeviceAudit();            // Writes a file DeviceAudit.txt to the current directory
-
             _rawinput.ButtonPressed += OnButtonPressed;
         }
-
         private void OnButtonPressed(object sender, RawInputEventArg e)
         {
             lbHandle.Text = e.ButtonPressEvent.DeviceHandle.ToString();
@@ -43,23 +30,9 @@ namespace Mouse
             usButtonFlags.Text = "usButtonFlags : " + e.ButtonPressEvent.usButtonFlags.ToString();
             usFlags.Text = "usFlags : " + e.ButtonPressEvent.usFlags.ToString();
         }
-
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             _rawinput.ButtonPressed -= OnButtonPressed;
-        }
-
-        private static void CurrentDomain_UnhandledException(Object sender, UnhandledExceptionEventArgs e)
-        {
-            var ex = e.ExceptionObject as Exception;
-
-            if (null == ex) return;
-
-            // Log this error. Logging the exception doesn't correct the problem but at least now
-            // you may have more insight as to why the exception is being thrown.
-            Debug.WriteLine("Unhandled Exception: " + ex.Message);
-            Debug.WriteLine("Unhandled Exception: " + ex);
-            MessageBox.Show(ex.Message);
         }
     }
 }
